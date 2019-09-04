eb14f43525e01868016d8230ec4993d271cf2d3f
wensug <wensug@gmail.com>
2019-09-04 12:37:57 +0100

    Initial Commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/bb_logo.jpg'
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className='nav'>
+        <div>
+          <img className="bb-logo" src={logo} alt='Butternut Box Logo'/>
+        </div> 
+            <p className='name-title' to='/'>
+                Wendy Gonzalez
+          </p>   
+      </div>
+    </header>
+  );
+}
